2d3319d41429c5a1483eaa58ea6483231549bae6
sebsong <sebsong@berkeley.edu>
2015-04-28 10:23:55 -0700

    Create form for new post, Add Foodstagram home button

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+  get 'new_post', to: 'posts#new'
+  post 'new_post', to: 'posts#create', as: :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427033436) do
+ActiveRecord::Schema.define(version: 20150428164020) do
 
   create_table "comments", force: true do |t|
     t.text     "message"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150427033436) do
     t.text     "caption"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
