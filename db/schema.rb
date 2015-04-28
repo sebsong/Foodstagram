830c1dec4c278a01f4cfa0c93d88419b9ce6f9e7
syoo11 <solahyoo@hotmail.com>
2015-04-28 19:17:34 +0000

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427033436) do
+ActiveRecord::Schema.define(version: 20150428031645) do
 
   create_table "comments", force: true do |t|
     t.text     "message"
