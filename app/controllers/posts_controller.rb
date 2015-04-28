b7b9f689c2aebdd978c26b2b2ea4ebf22c2e8e51
sebsong <sebsong@berkeley.edu>
2015-04-28 10:21:24 -0700

    Create form for new post, Add Foodstagram home button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+    def new
+        @user = current_user
+        @post = @user.post.build
+    end
+
+    def create
+        @user = current_user
+        @post = @user.post.build post_params
+        if @post.save
+            redirect_to user_path(@user.id)
+        else
+            flash[:error] = @post.errors.full_messages.to_sentence
+            render :action => "new"
+        end
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:caption)
+    end
 end
