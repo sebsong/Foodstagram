e3d2fd9423841fe856add1cc855f1d10936277f5
sebsong <sebsong@berkeley.edu>
2015-04-28 12:19:58 -0700

    Add title to form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,6 @@ class PostsController < ApplicationController
 
     private
     def post_params
-        params.require(:post).permit(:caption)
+        params.require(:post).permit(:title, :caption)
     end
 end
