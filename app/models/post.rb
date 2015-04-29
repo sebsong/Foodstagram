4bc80e0b32b2d46d7593d19fe641c551a0acc09e
syoo11 <solahyoo@hotmail.com>
2015-04-29 02:34:41 +0000

    added time on feed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,11 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 	has_many :votes
-	has_attached_file :avatar, styles: { small: "64x64", med: "100x100", large: "200x200" }
+	has_attached_file :avatar, :styles => {
+      :thumb => "100x100#",
+      :small  => "150x150>",
+      :medium => "200x200",
+      :large => "1000x1000"}
 	validates_attachment :avatar,
 	    :presence => true,
 	    :size => { :in => 0..10.megabytes },
